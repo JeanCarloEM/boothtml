fd33ecad956f742508a8de0d97d52793bb1cb523
Jean Carlo EM <jeancarlo@jeancarloem.com>
2023-07-21 05:43:34 -0300

    removido build errados

--- dist/stringTransform/definitions.js
+++
@@ -1,6 +0,0 @@
-export var PromiseExecutionMode;
-(function (PromiseExecutionMode) {
-    PromiseExecutionMode[PromiseExecutionMode["All"] = 0] = "All";
-    PromiseExecutionMode[PromiseExecutionMode["ForEach"] = 1] = "ForEach";
-})(PromiseExecutionMode || (PromiseExecutionMode = {}));
-//# sourceMappingURL=definitions.js.map
